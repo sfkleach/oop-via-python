38d787a3ca734818c26b50467d0d3aefebd4d4c4
Stephen Leach <sfkleach@gmail.com>
2021-11-01 14:27:53 +0000

    Make suitable for class exercise

--- session04/purse_4_A.py
+++ session04/purse_4_A.py
@@ -8,13 +8,13 @@ class Purse:
         """
         self._cashmap = { fv:n for (fv, n) in initial_cashmap.items() if isinstance(fv, int) and isinstance(n, int) }
 
-    def _ensure(self, face_values ):                # Q: Why private?
+    def ensure(self, face_values ):                # Q: Why private?
         for fv in face_values:
             if fv not in self._cashmap:
                 self._cashmap[fv] = 0
 
-    def _addm(self, cashmap, multiplier):           # Q: Why private?
-        self._ensure(cashmap.keys())
+    def addm(self, cashmap, multiplier):           # Q: Why private?
+        self.ensure(cashmap.keys())
         for (fv, n) in cashmap.items():
             self._cashmap[fv] += multiplier * n
 
@@ -22,16 +22,16 @@ class Purse:
         """Returns the total value of a purse"""
         return sum(fv * n for (fv, n ) in self._cashmap.items())
 
-    def _can_pay(self, amount):                     # Q: Why private?
+    def can_pay(self, amount):                     # Q: Why private?
         return self.total() >= amount
 
     def pay_in(self, other_purse):
         """Drains the coins from the other_purse into this one"""
         cash = other_purse._cashmap                 # Q: Is this OK?
         other_purse = {}
-        self._addm(cash, 1)
+        self.addm(cash, 1)
 
-    def _cash_list(self):
+    def cash_list(self):
         """Return the cashmap as a list of tuples sorted by face-value"""
         return [(k, self._cashmap[k]) for k in sorted(self._cashmap.keys(), reverse=True)]
 
@@ -44,10 +44,10 @@ class Purse:
 
         If we cannot make a good offer, return None.
         """
-        if not self._can_pay(amount):
+        if not self.can_pay(amount):
             return None
-        cashmap = self._generate_offer_cashmap(amount, math.inf)
-        return self._split_purse(cashmap)
+        cashmap = self.(amount, math.inf)
+        return self.split_purse(cashmap)
 
     def please_pay_exactly(self, amount):
         """
@@ -56,24 +56,24 @@ class Purse:
         those in this purse. This purse must be debited by the coins of the
         change purse.
         """
-        if not self._can_pay(amount):
+        if not self.can_pay(amount):
             return None
-        cashmap = self._generate_offer_cashmap(amount, 0)
-        return self._split_purse(cashmap)
+        cashmap = self.generate_offer_cashmap(amount, 0)
+        return self.split_purse(cashmap)
 
-    def _split_purse(self, cashmap):
+    def split_purse(self, cashmap):
         if cashmap is not None:
             offer_purse = Purse()
-            offer_purse._addm(cashmap, 1)
-            self._addm(cashmap, -1)
+            offer_purse.addm(cashmap, 1)
+            self.addm(cashmap, -1)
             return offer_purse
 
-    def _generate_offer_cashmap(self, amount, best_offer_excess):
+    def generate_offer_cashmap(self, amount, best_offer_excess):
         """Returns the best offer cashmap"""
-        cash_list = self._cash_list()
+        cash_list = self.()
         available_total = self.total()
         best_offer_cashmap = None
-        for (excess, cashmap) in Purse._find_offers(cash_list, available_total, amount, {}):
+        for (excess, cashmap) in Purse.find_offers(cash_list, available_total, amount, {}):
             print("solution", excess, cashmap)
             if excess < best_offer_excess:
                 best_offer_excess = excess
@@ -85,7 +85,7 @@ class Purse:
         return best_offer_cashmap
 
     @staticmethod
-    def _find_offers(available_cashlist, available_total, amount_remaining, sofar_cashmap):
+    def find_offers(available_cashlist, available_total, amount_remaining, sofar_cashmap):
         """Generator that finds solutions of the form (offer_excess, offer_cashmap)"""
         print("here", available_cashlist, available_total, amount_remaining, sofar_cashmap)
         if amount_remaining <= 0:
@@ -99,7 +99,7 @@ class Purse:
             for i in range(min(k, n), -1, -1):   # Count down from n+1 to 0
                 value_total = fv * n
                 value_to_add = fv * i
-                yield from Purse._find_offers(available_cashlist[1:], available_total - value_total, amount_remaining - value_to_add, {fv:i, **sofar_cashmap})
+                yield from Purse.find_offers(available_cashlist[1:], available_total - value_total, amount_remaining - value_to_add, {fv:i, **sofar_cashmap})
 
 def transaction(target_amount, vendor_purse, customer_purse):
     success = False
@@ -110,7 +110,7 @@ def transaction(target_amount, vendor_purse, customer_purse):
         offer_purse_total = offer_purse.total()
         if offer_purse_total < target_amount:
             return False
-        change_purse = vendor_purse.please_pay_exactly(offer_amount - target_amount) or Purse()
+        change_purse = vendor_purse.please_pay_exactly(offer_purse_total - target_amount) or Purse()
         if offer_purse_total != target_amount + change_purse.total():
             return False
         vendor_purse.pay_in(offer_purse)
