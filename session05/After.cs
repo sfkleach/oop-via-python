1dcf41b2b7659decac0c3a4db16b6f963534965c
Stephen Leach <sfkleach@gmail.com>
2021-11-14 15:12:25 +0000

    Add C# examples

---
+++ session05/After.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise {
+
+    public class ArticleStatus {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Article {
+        public bool? HiddenByReviewer { get; set; }
+        public ArticleStatus Status { get; set; }
+        public long ReviewCode { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserFilter {
+        public string Value { get; set; }
+        public string Logic { get; set; }
+        public string Operator { get; set; }
+
+        public bool IsBasicFilter() {
+            return Logic.Equals("or") && Operator.Equals("contains");
+        }
+
+        public IEnumerable<Article> RunFilter(IEnumerable<Article> data) {
+            if (IsBasicFilter()) {
+                return(
+                    data.Where(
+                        t => ( 
+                            t.Name.Contains(this.Value, StringComparison.OrdinalIgnoreCase) ||
+                            string.Format("THING#{0}", t.ReviewCode.ToString().PadLeft(10, '0')).Contains(this.Value, StringComparison.OrdinalIgnoreCase)
+                        )
+                    )
+                );
+            } else {
+                return data;
+            }
+        }
+    }
+    
+    public enum Tab {
+        WorkInProgress,
+        SubmittedForReview,
+        Accepted,
+        Completed
+    }
+
+    public class Column {
+        public static Column Make(Tab col) {
+            switch (col) {
+                case Tab.Accepted: return new AcceptedColumn();
+                case Tab.Completed: return new CompletedColumn();
+                default: return new Column();
+            }
+        }
+        public virtual IEnumerable<Article> RunColumnFilter(IEnumerable<Article> data) {
+            return data;
+        }
+        public virtual IEnumerable<Article> RunUserFilter(IEnumerable<Article> data, UserFilter user_filter) {
+            return data;
+        }
+        public IEnumerable<Article> RunBothFilters(IEnumerable<Article> data, UserFilter user_filter ) {
+            return RunUserFilter(RunColumnFilter(data), user_filter);
+        }
+    }
+
+    public abstract class FilteringColumn : Column {
+        public override IEnumerable<Article> RunUserFilter(IEnumerable<Article> data, UserFilter user_filter) {
+            return user_filter.RunFilter(data);
+        }
+    }
+
+    public class AcceptedColumn : FilteringColumn {
+        public override IEnumerable<Article> RunColumnFilter(IEnumerable<Article> data) {
+            return data.Where(p => !p.HiddenByReviewer.HasValue || !p.HiddenByReviewer.Value);
+        }
+    }
+
+    public class CompletedColumn : FilteringColumn {
+        public override IEnumerable<Article> RunColumnFilter(IEnumerable<Article> data) {
+            return(
+                data.Where(
+                    p => (
+                        (p.HiddenByReviewer.HasValue == true && p.HiddenByReviewer.Value == true) ||
+                        (p.Status != null && p.Status.Id == 9)
+                    )
+                )
+            );
+        }
+    }
+
+    public class Exercise {
+        private static IEnumerable<Article> FilterProposals(IEnumerable<Article> dataToFilter, UserFilter user_filter, Column column) {
+            if (user_filter == null || column == null) throw new ArgumentNullException();
+            return column.RunBothFilters(dataToFilter, user_filter);
+        }       
+        public static IEnumerable<Article> FilterProposals(IEnumerable<Article> dataToFilter, UserFilter user_filter, Tab col) {
+            return FilterProposals(dataToFilter, user_filter ?? new UserFilter(), Column.Make(col));
+        }
+    }
+}
---
+++ session05/Before.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeforeAndAfter.Before {
+
+    public class ArticleStatus {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Article {
+        public bool? HiddenByReviewer { get; set; }
+        public ArticleStatus Status { get; set; }
+        public long ReviewCode { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserFilter {
+        public string Value { get; set; }
+        public string Logic { get; set; }
+        public string Operator { get; set; }
+    }
+    
+    public enum Tab {
+        WorkInProgress,
+        SubmittedForReview,
+        Accepted,
+        Completed
+    }
+
+    public class Exercise {
+
+        public IEnumerable<Article> FilterProposals(IEnumerable<Article> dataToFilter, UserFilter filter, Tab tab)
+        {
+            IEnumerable<Article> filteredData = null;
+
+            switch (tab) {
+                case Tab.Accepted:
+                    filteredData = dataToFilter.Where(p => !p.HiddenByReviewer.HasValue || !p.HiddenByReviewer.Value);
+                    break;
+                case Tab.Completed:
+                    filteredData = (
+                        dataToFilter.Where(
+                            p => (
+                                (p.HiddenByReviewer.HasValue == true && p.HiddenByReviewer.Value == true) ||
+                                (p.Status != null && p.Status.Id == 9)
+                        
+                            )    
+                        )
+                    );
+                    break;
+                default:
+                    break;
+            }
+
+            if (filter != null && !string.IsNullOrEmpty(filter.Value))
+            {
+                var tmpData = filteredData ?? dataToFilter;
+
+                if (filter.Logic.Equals("or") && filter.Operator.Equals("contains"))
+                {
+                    switch (tab)
+                    {
+                        case Tab.Accepted:
+                        case Tab.Completed:
+                            filteredData = tmpData.Where(t => t.Name.IndexOf(filter.Value, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                            string.Format("THING#{0}", t.ReviewCode.ToString().PadLeft(10, '0')).IndexOf(filter.Value, StringComparison.OrdinalIgnoreCase) >= 0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return filteredData;
+        }
+
+    }
+}
