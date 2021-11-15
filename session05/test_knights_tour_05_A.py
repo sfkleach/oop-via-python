e7d06650a09c863d7186c81c9628746bf4b6c2b5
Stephen Leach <sfkleach@gmail.com>
2021-11-15 23:42:20 +0000

    Renaming Knight's Tour files

--- session05/test_knights_tour_05_A.py
+++ session05/test_knights_tour_05_A.py
@@ -1,10 +1,7 @@
-from alt_knights_tour import *
+from knights_tour_05_A import *
 
 def test_knights_tour():
-    # Arrange
     size = 20
-    # Act
     board = knights_tour('a1', boardsize=size)
-    # Assert
     assert sorted(board.values()) == list(range(1, size**2 + 1))
     assert sorted(board.keys()) == list((x, y) for x in range(0, size) for y in range(0, size))
--- session05/test_knights_tour_05_B.py
+++ session05/test_knights_tour_05_B.py
@@ -1,7 +1,10 @@
-from knights_tour import *
+from knights_tour_05_B import *
 
 def test_knights_tour():
+    # Arrange
     size = 20
+    # Act
     board = knights_tour('a1', boardsize=size)
+    # Assert
     assert sorted(board.values()) == list(range(1, size**2 + 1))
     assert sorted(board.keys()) == list((x, y) for x in range(0, size) for y in range(0, size))
