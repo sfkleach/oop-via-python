91d176687009b827b2703e9b28440d616ce3c652
Stephen Leach <sfkleach@gmail.com>
2022-01-09 16:29:46 +0000

    Initial commit of our example illustrating a basic error

---
+++ session07/README.md
@@ -0,0 +1,7 @@
+# When is an Object _not_ an Object?
+
+## Example: TreeMaker
+
+ * The file treemaker.py illustrates a basic mistake. The exercise is to identify the mistake. (Arguably there are 2 errors here insofar there's undocumented restriction on make.)
+ * `% python3 -i treemaker.py` will load the code, run and print the example & leave us at an interactive prompt
+ * 
---
+++ session07/treemaker.py
@@ -0,0 +1,45 @@
+# m = TreeMaker()
+# m.add( key: str, value: Any )
+# m.start( key: str )
+# m.end()
+# m.make() -> Dict[str, Any]
+
+from typing import Dict, Any
+
+class TreeMaker:
+
+    def __init__( self ):
+        self._focus = {}
+        self._dump = []
+
+    def add( self, **kwargs ):
+        self._focus.update( kwargs )
+
+    def start( self, key: str ):
+        new = {}
+        self._focus[ key ] = new
+        self._dump.append( self._focus )
+        self._focus = new
+
+    def end( self ):
+        self._focus = self._dump.pop()
+
+    def make( self ) -> Dict[str, Any]:
+        return self._focus
+
+if __name__ == "__main__":
+    m = TreeMaker()
+    m.add( name = 'Stephen' )
+    m.add( age = 62 )
+    m.start( 'activities' )
+    m.start( 'coding' )
+    m.add( duration = 45 )
+    m.end()
+    m.start( 'birdwatching' )
+    m.add( duration = 40 )
+    m.end()
+    m.end()
+    tree = m.make()
+    print( tree )
+    # {'name': 'Stephen', 'age': 62, 'activities': {'coding': {'duration': 45}, 'birdwatching': {'duration': 40}}}
+
